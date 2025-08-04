5076cb682a3a5d3335c15b327ef1b637cde9f8c0
Nikitha2404 <hebbar.nikitha@gmail.com>
2025-08-04 19:58:16 +0530

    added CRUD endpoints

--- internal/db.go
+++ internal/db.go
@@ -25,3 +25,31 @@ func (d *DB) AddOrder(order *orders.Order) error {
 	d.collection = append(d.collection, order)
 	return nil
 }
+
+func (d *DB) GetOrder(orderId uint64) *orders.Order {
+	for _, o := range d.collection {
+		if o.OrderId == orderId {
+			return o
+		}
+	}
+	return nil
+}
+
+func (d *DB) DeleteOrder(orderId uint64) {
+	filtered := make([]*orders.Order, len(d.collection)-1)
+	for i := range d.collection {
+		if d.collection[i].OrderId != orderId {
+			filtered = append(filtered, d.collection[i])
+		}
+	}
+	d.collection = filtered
+}
+
+func (d *DB) UpdateOrder(order *orders.Order) {
+	for i, o := range d.collection {
+		if o.OrderId == order.OrderId {
+			d.collection[i] = order
+			return
+		}
+	}
+}
--- internal/orderservice.go
+++ internal/orderservice.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/Nikitha2404/go-grpc-api/protogen/golang/orders"
@@ -19,8 +20,41 @@ func NewOrderService(db *DB) OrderService {
 }
 
 func (o *OrderService) AddOrder(_ context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
+	log.Println("Exisiting Data: ", o.db.collection)
 	log.Println("Recieved an add-order request")
 
 	err := o.db.AddOrder(req.GetOrder())
+	log.Println("Exisiting Data: ", o.db.collection)
 	return &orders.Empty{}, err
+}
+
+func (o *OrderService) DeleteOrder(_ context.Context, req *orders.PayloadWithOrderId) (*orders.Empty, error) {
+	log.Println("Exisiting Data: ", o.db.collection)
+	log.Println("Recieved a delete-order request")
+
+	o.db.DeleteOrder(req.OrderId)
+	log.Println("Exisiting Data: ", o.db.collection)
+	return &orders.Empty{}, nil
+}
+
+func (o *OrderService) GetOrder(_ context.Context, req *orders.PayloadWithOrderId) (*orders.PayloadWithSingleOrder, error) {
+	log.Println("Exisiting Data: ", o.db.collection)
+	log.Println("Received get-order request")
+
+	orderDetails := o.db.GetOrder(req.OrderId)
+	if orderDetails == nil {
+		return nil, fmt.Errorf("order details not found for id=%d", req.OrderId)
+	}
+	return &orders.PayloadWithSingleOrder{
+		Order: orderDetails,
+	}, nil
+}
+
+func (o *OrderService) UpdateOrder(_ context.Context, req *orders.PayloadWithSingleOrder) (*orders.Empty, error) {
+	log.Println("Exisiting Data: ", o.db.collection)
+	log.Println("Recieved update-order request")
+
+	o.db.UpdateOrder(req.GetOrder())
+	log.Println("Exisiting Data: ", o.db.collection)
+	return &orders.Empty{}, nil
 }
